6860d17c7c6ddbf6d8ba1952ceb612e4e5e517bf
Angela-GM <angelagm0112@gmail.com>
2023-05-03 00:21:43 +0200

    modal signup

---
+++ index.js
@@ -0,0 +1,19 @@
+const signupBtn = document.getElementById("signup-btn");
+const modalOverlay = document.getElementById("modal-overlay");
+const modal = document.getElementById("modal");
+const cancelBtn = document.getElementById("cancel-btn");
+
+signupBtn.addEventListener("click", function () {
+  modalOverlay.style.display = "block";
+  modal.style.display = "block";
+});
+
+modalOverlay.addEventListener("click", function () {
+  modalOverlay.style.display = "none";
+  modal.style.display = "none";
+});
+
+cancelBtn.addEventListener("click", function () {
+  modalOverlay.style.display = "none";
+  modal.style.display = "none";
+});
